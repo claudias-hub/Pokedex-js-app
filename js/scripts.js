af3dc25bbbb964ce62e473cc881314207a4a2ea9
claudias-hub <c_oka82@hotmail.com>
2025-01-24 16:19:37 +0100

    Create a variable name pokemonList and Add 7 object to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-alert('Hello world');
-
-let favoriteFood = "rice";
-document.write("My favorite food is " + favoriteFood);
+let pokemonList = [
+    {
+        name: "Arbok",
+        height: 3.5,
+        types: ["Poison"]
+      },
+    {
+        name: "Pikachu",
+        height: 0.4, 
+        types: ["Electric"]
+      },
+    {
+        name: "Charmeleon",
+        height: 1.1, 
+        types: ["Fire"]
+      },
+    {
+        name: "Ninetales",
+        height: 1.1, 
+        types: ["Fire"]
+      },
+    {
+        name: "Clefable",
+        height: 1.3, 
+        types: ["Fairy"]
+      },
+    {
+        name: "Electrode",
+        height: 1.2, 
+        types: ["Electric"]
+      },
+    {
+        name: "Pidgeot",
+        height: 1.5, 
+        types: ["Flying", "Normal"]
+      },
+      
+]
