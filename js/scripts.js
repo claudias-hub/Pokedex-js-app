f5ced2be1a72232fcc83aeed8b7b9959b8b85957
claudias-hub <c_oka82@hotmail.com>
2025-02-14 19:50:02 +0100

    remove the array of Pokémon objects and replace it with an empty array

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Define an array containing Pokémon data
     let pokemonList = [
-      { name: "Arbok", height: 3.5, types: ["Poison"] },
-      { name: "Pikachu", height: 0.4, types: ["Electric"] },
-      { name: "Charmeleon", height: 1.1, types: ["Fire"] },
-      { name: "Ninetales", height: 1.1, types: ["Fire"] },
-      { name: "Clefable", height: 1.3, types: ["Fairy"] },
-      { name: "Electrode", height: 1.2, types: ["Electric"] },
-      { name: "Pidgeot", height: 1.7, types: ["Flying", "Normal"] }
+      
     ];
 
     // Define a height threshold value for Pokémon
